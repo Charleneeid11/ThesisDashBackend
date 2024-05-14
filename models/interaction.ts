50dc70e0f01d63ea07ef532c31352e9524fa27c5
Charleneeid11 <charlene.elkhouryeid@gmail.com>
2024-05-14 13:40:27 +0300

    Added fix

--- models/interaction.ts
+++ models/interaction.ts
@@ -3,9 +3,9 @@ import type { Interaction } from "../interfaces/Interaction"
 
 const interactionSchema = new Schema<Interaction> (
     {
-        _id: { type: mongoose.Schema.Types.ObjectId },
-        uid: { type: String },
-        itype: { type: String}
+        _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+        uid: { type: String, required: true },
+        itype: { type: String, required: true }
     }
 )
 
